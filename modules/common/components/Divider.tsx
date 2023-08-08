87f27b33b0681e1f32278e859e666371a4425f94
Thomas Min <gyeongmin.min@ssupport.co.kr>
2023-08-09 00:55:41 +0900

    feat : set common module

---
+++ modules/common/components/Divider.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export const Divider = function Divider() {
+  return <View className={`border-b border-px border-gray-200`}></View>;
+};
