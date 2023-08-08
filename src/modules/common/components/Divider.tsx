a437ae133277794f230f1e168cdca0e6c9956948
Thomas Min <gyeongmin.min@ssupport.co.kr>
2023-08-09 00:51:19 +0900

    fix : remove unused

--- src/modules/common/components/Divider.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { View } from 'react-native';
-import clsx from 'clsx';
-
-export const Divider = function Divider({ className }: { className?: string }) {
-  return <View className={clsx(`border-b border-px border-gray-300`, className)}></View>;
-};
--- src/modules/workout/components/WorkoutExerciseList.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { View } from 'react-native';
-
-export const WorkoutExerciseList = function WorkoutExerciseList() {
-  return <View className="flex-1 bg-white"></View>;
-};
