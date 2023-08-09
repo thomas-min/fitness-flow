6c28533e5bfb8e0f8441ba18ebcf8208668d2443
Thomas Min <gyeongmin.min@ssupport.co.kr>
2023-08-09 23:07:47 +0900

    feat : set title text to center

--- modules/exercise/components/ExerciseScreenHeader.tsx
+++ modules/exercise/components/ExerciseScreenHeader.tsx
@@ -7,9 +7,9 @@ export const ExerciseScreenHeader = function WorkoutScreenHeader() {
   const { toggleMode } = useExerciseViewActions();
 
   return (
-    <View className="flex-row justify-between items-center px-4 pt-2">
-      <Text className="text-xl font-bold">운동</Text>
-      <Pressable onPress={toggleMode} className="active:bg-gray-100 p-2 rounded">
+    <View className="flex-row relative justify-center items-center px-4 pt-2">
+      <Text className="text-xl text-center font-bold">운동</Text>
+      <Pressable onPress={toggleMode} className="absolute right-4 active:opacity-70 p-2 rounded">
         <Text className="text-gray-500">편집</Text>
       </Pressable>
     </View>
