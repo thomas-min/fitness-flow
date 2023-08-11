106d19021c8222e4a5b38899d847ebc83efd1748
Thomas Min <gyeongmin.min@ssupport.co.kr>
2023-08-11 21:40:49 +0900

    feat : add db migration statements

---
+++ modules/common/db/index.ts
@@ -0,0 +1,34 @@
+import { IStatement, Migrations, sql } from 'expo-sqlite-orm';
+
+export const databaseName = 'fitnessFlow';
+
+// TODO: add seed data
+export const statements: IStatement = {
+  '1662689376195_create_exercises': sql`
+        CREATE TABLE exercises (
+          id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+          bodyPart TEXT NOT NULL,
+          name TEXT NOT NULL,
+          unit TEXT NOT NULL,
+          isDeleted BOOLEAN NOT NULL DEFAULT false
+        );`,
+  '1662689376196_create_routines': sql`
+        CREATE TABLE routines (
+          id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+          name TEXT NOT NULL,
+          position INTEGER NOT NULL,
+          isDeleted BOOLEAN NOT NULL DEFAULT false
+        );`,
+  '1662689376197_create_routine_exercises': sql`
+        CREATE TABLE routineExercises (
+          id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+          routineId INTEGER NOT NULL,
+          exerciseId INTEGER NOT NULL,
+          position INTEGER NOT NULL,
+          isDeleted BOOLEAN NOT NULL DEFAULT false,
+          FOREIGN KEY (routineId) REFERENCES routines (id),
+          FOREIGN KEY (exerciseId) REFERENCES exercises (id)
+        );`,
+};
+
+export const migrations = new Migrations(databaseName, statements);
