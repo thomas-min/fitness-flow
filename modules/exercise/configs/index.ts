0a50775ac76956fe3c2849aa21a632ef452ffe4a
Thomas Min <gyeongmin.min@ssupport.co.kr>
2023-08-11 21:42:53 +0900

    fix : update default exercise to array

--- modules/exercise/configs/index.ts
+++ modules/exercise/configs/index.ts
@@ -14,95 +14,95 @@ export const BODY_PARTS_IN_KOREAN: Record<TBodyPart, string> = {
   chest: '가슴',
   back: '등',
   shoulders: '어깨',
-  legs: '다리',
+  legs: '하체',
   arms: '팔',
   abs: '복근',
   whole: '전신',
 };
 
-export const DEFAULT_EXERCISES: Record<string, IExercise> = {
-  'bench-press': {
-    id: 'bench-press',
+export const DEFAULT_EXERCISES: IExercise[] = [
+  {
+    id: 1,
     name: '벤치프레스',
     bodyPart: 'chest',
     unit: 'kg',
   },
-  squat: {
-    id: 'squat',
+  {
+    id: 2,
     name: '스쿼트',
     bodyPart: 'legs',
     unit: 'kg',
   },
-  'dead-lift': {
-    id: 'dead-lift',
+  {
+    id: 3,
     name: '데드리프트',
     bodyPart: 'legs',
     unit: 'kg',
   },
-  'overhead-press': {
-    id: 'overhead-press',
+  {
+    id: 4,
     name: '오버헤드프레스',
     bodyPart: 'shoulders',
     unit: 'kg',
   },
-  'barbel-row': {
-    id: 'barbel-row',
+  {
+    id: 5,
     name: '바벨로우',
     bodyPart: 'back',
     unit: 'kg',
   },
-  'pull-up': {
-    id: 'pull-up',
+  {
+    id: 6,
     name: '풀업',
     bodyPart: 'back',
     unit: 'kg',
   },
-  'push-up': {
-    id: 'push-up',
+  {
+    id: 7,
     name: '푸쉬업',
     bodyPart: 'chest',
     unit: 'kg',
   },
-  'side-lateral-raise': {
-    id: 'side-lateral-raise',
+  {
+    id: 8,
     name: '사이드레터럴레이즈',
     bodyPart: 'shoulders',
     unit: 'kg',
   },
-  'barbel-curl': {
-    id: 'exercise-9',
+  {
+    id: 9,
     name: '바벨컬',
     bodyPart: 'arms',
     unit: 'kg',
   },
-  'skull-crusher': {
-    id: 'exercise-10',
+  {
+    id: 10,
     name: '스컬크러셔',
     bodyPart: 'arms',
     unit: 'kg',
   },
-  'ab-crunch': {
-    id: 'exercise-11',
+  {
+    id: 11,
     name: '크런치',
     bodyPart: 'abs',
     unit: 'kg',
   },
-  'leg-raise': {
-    id: 'exercise-12',
+  {
+    id: 12,
     name: '레그레이즈',
     bodyPart: 'abs',
     unit: 'kg',
   },
-  'clean-and-jerk': {
-    id: 'clean-and-jerk',
+  {
+    id: 13,
     name: '클린앤저크',
     bodyPart: 'whole',
     unit: 'kg',
   },
-  snatch: {
-    id: 'snatch',
+  {
+    id: 14,
     name: '스내치',
     bodyPart: 'whole',
     unit: 'kg',
   },
-};
+];
