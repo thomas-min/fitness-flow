2a3c5d62c01325057d8f02c74ea0e2ca4d9f1c3a
Thomas Min <gyeongmin.min@ssupport.co.kr>
2023-08-11 21:42:07 +0900

    feat : add exercise respository

--- modules/exercise/models/index.ts
+++ modules/exercise/models/index.ts
@@ -1,9 +1,21 @@
+import { ColumnMapping, Repository, columnTypes } from 'expo-sqlite-orm';
+
 export type TBodyPart = 'chest' | 'back' | 'shoulders' | 'legs' | 'arms' | 'abs' | 'whole';
 
 export interface IExercise {
-  id: string;
+  id: number;
   bodyPart: TBodyPart;
   name: string;
   unit: string;
   isDeleted?: boolean;
 }
+
+const columnMapping: ColumnMapping<IExercise> = {
+  id: { type: columnTypes.TEXT },
+  bodyPart: { type: columnTypes.TEXT },
+  name: { type: columnTypes.TEXT },
+  unit: { type: columnTypes.TEXT },
+  isDeleted: { type: columnTypes.BOOLEAN, default: () => false },
+};
+
+export const exerciseRepository = new Repository('fitnessFlow', 'exercises', columnMapping);
