79e3f85443c5213219c086bf8f024574e81a7222
Thomas Min <gyeongmin.min@ssupport.co.kr>
2023-08-11 21:41:19 +0900

    feat : add routine repository

--- modules/routine/models/index.ts
+++ modules/routine/models/index.ts
@@ -1,14 +1,39 @@
+import { ColumnMapping, Repository, columnTypes } from 'expo-sqlite-orm';
+
 export interface IRoutine {
-  id: string;
+  id: number;
   name: string;
-  order: number;
+  position: number;
   isDeleted?: boolean;
 }
 
+const routineColumnMapping: ColumnMapping<IRoutine> = {
+  id: { type: columnTypes.INTEGER },
+  name: { type: columnTypes.TEXT },
+  position: { type: columnTypes.INTEGER },
+  isDeleted: { type: columnTypes.BOOLEAN, default: () => false },
+};
+
+export const routineRepository = new Repository('fitnessFlow', 'routines', routineColumnMapping);
+
 export interface IRoutineExercise {
-  id: string;
-  routineId: string;
-  exerciseId: string;
-  order: number;
+  id: number;
+  routineId: number;
+  exerciseId: number;
+  position: number;
   isDeleted?: boolean;
 }
+
+const routineExerciseColumnMapping: ColumnMapping<IRoutineExercise> = {
+  id: { type: columnTypes.INTEGER },
+  routineId: { type: columnTypes.INTEGER },
+  exerciseId: { type: columnTypes.INTEGER },
+  position: { type: columnTypes.INTEGER },
+  isDeleted: { type: columnTypes.BOOLEAN, default: () => false },
+};
+
+export const routineExerciseRepository = new Repository(
+  'fitnessFlow',
+  'routineExercises',
+  routineExerciseColumnMapping
+);
