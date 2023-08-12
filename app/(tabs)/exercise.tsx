e4c81b20a4ca1dbc4fa012bea999cec5a8602912
Thomas Min <gyeongmin.min@ssupport.co.kr>
2023-08-12 11:41:39 +0900

    remove : seed effect from exercise screen

--- app/(tabs)/exercise.tsx
+++ app/(tabs)/exercise.tsx
@@ -1,5 +1,5 @@
 import { useNavigation } from 'expo-router';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Platform, UIManager } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -7,8 +7,9 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { BodyPartSection } from '@/modules/exercise/components/BodyPartSection';
 import { ExerciseScreenHeader } from '@/modules/exercise/components/ExerciseScreenHeader';
 import { BODY_PARTS } from '@/modules/exercise/configs';
-import { useExerciseModelActions } from '@/modules/exercise/hooks/useExerciseModelStore';
-import { useExerciseViewActions } from '@/modules/exercise/hooks/useExerciseViewStore';
+import { useExerciseViewActions } from '@/modules/exercise/hooks/useExerciseScreenStore';
+import { IExercise } from '@/modules/exercise/models';
+import { getExercises } from '@/modules/exercise/utils';
 
 if (Platform.OS === 'android') {
   UIManager.setLayoutAnimationEnabledExperimental &&
@@ -17,23 +18,36 @@ if (Platform.OS === 'android') {
 
 export default function ExerciseScreen() {
   const { setMode } = useExerciseViewActions();
-  const { seed } = useExerciseModelActions();
   const navigation = useNavigation();
 
+  const [exercises, setExercises] = useState<IExercise[]>([]);
+
   useEffect(() => {
-    const unsubscribe = navigation.addListener('focus', () => {
+    const unsubscribe = navigation.addListener('focus', async () => {
       setMode('view');
-      seed();
+      setExercises(
+        await getExercises({
+          order: {
+            id: 'ASC',
+          },
+        })
+      );
     });
     return unsubscribe;
-  }, [navigation, seed, setMode]);
+  }, [navigation, setMode]);
 
   return (
     <SafeAreaView className="flex-1 bg-white" edges={['top']}>
       <ExerciseScreenHeader />
+      {/* TODO: replace with section list */}
       <ScrollView className="flex-1 px-4">
         {BODY_PARTS.map((bodyPart) => (
-          <BodyPartSection key={bodyPart} bodyPart={bodyPart} />
+          <BodyPartSection
+            key={bodyPart}
+            bodyPart={bodyPart}
+            exercises={exercises}
+            setExercises={setExercises}
+          />
         ))}
       </ScrollView>
     </SafeAreaView>
